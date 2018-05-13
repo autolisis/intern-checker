6f0369cda7d8599824a5701e09205966811cf84d
reisub0 <reisub0@gmail.com>
2018-05-13 17:17:29 +0530

    Clean-up and comments

--- intern-checker.py
+++ intern-checker.py
@@ -2,27 +2,46 @@
 
 from selenium import webdriver
 import bs4
-# import pickle
 from time import sleep
-# import pdb
+from os.path import expanduser
 
+# Set the command line options for chromium web driver
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('user-data-dir=/home/g/temp/intern/Default')
+
+# Change this to your chrome data dir usually ~/.config/chromium/ on *nix
+path = '~/.config/chromium'
+chrome_options.add_argument(f'user-data-dir={expanduser(path)}')
+
+# Sets X-Server WM - Class, can write code to minimize it so it appears headless
 chrome_options.add_argument('class=selenium-chrome')
+
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
 driver.get('http://www.internshala.com')
 
+# Sleep for a while to ensure that the entire page is loaded
 sleep(2)
-html = driver.page_source
-driver.close()
-soup = bs4.BeautifulSoup(html, 'lxml')
 
-for tag in soup.find_all('div'):
+# Get the page source and begin parsing with BS4
+soup = bs4.BeautifulSoup(driver.page_source, 'lxml')
+
+# We no longer need the chrome window
+driver.close()
+
+
+# Main parser function that gets the relevant detail from a div tag
+def getInternStatus(tag):
     try:
-        tagClass = tag['class'][0]
+        if tag['class'][0] == 'tooltiptextcontainer':
+            return tag.contents[0]
     except:
-        continue
-    if tagClass == 'tooltiptextcontainer':
-        print(tag.contents[0], end=' ')
-print()
+        return None
+    return None
+
+
+# Pass a list of all the div tags to this function and then filter out all the exceptional cases
+internStatuses = list(filter(None, map(getInternStatus, soup.find_all('div'))))
+
+
+# Print out all the required intern statuses
+print(*internStatuses, sep=' | ')
